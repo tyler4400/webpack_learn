5f2a91f4a842999745c828abbdd64a7bc9fede4b
tylerzzheng <tylerzzheng@tencent.com>
2021-05-07 19:16:23 +0800

    压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
 console.log('resolve', path.resolve());
 console.log('__dirname', path.join(__dirname, './dist'));
@@ -21,12 +25,16 @@ const config = {
         port: 8060
     },
 
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCssAssetsWebpackPlugin({})]
+    },
+
     module: {
         rules: [
             {
                 // sass-loader node-sass两个依赖都需要安装
                 test: /\.(sass|scss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
@@ -44,7 +52,18 @@ const config = {
             filename: "index.html",
             template: "template.html"
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new copyWebpackPlugin({
+            patterns:[{
+                from: path.join(__dirname, 'assets'),
+                to: 'assets',
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
     ]
 }
 
